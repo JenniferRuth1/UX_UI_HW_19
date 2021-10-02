411fb13e9df9469e38c815fc2064a29ab75927ec
JenniferRuth1 <jlrountree@yahoo.com>
2021-10-02 10:46:29 -0500

    Update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
-$(function () {
-    $('.SendEmail').click(function (event) {
-      var email = 'jenniferruthdesign@gmail.com';
-      var subject = 'Test';
-      var emailBody = 'Hi Sample,';
-      var attach = 'path';
-      document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody+
-          "?attach="+attach;
-    });
-  });
+function fade() {
+  $('.preloader').fadeOut("slow");
+  }
+  setTimeout(fade, 3000);
+  
